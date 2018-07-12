b1d13af77c5dd4959a46ceba8fc238789c0cbab7
Hayden Betts <haydenbetts@gmail.com>
2018-07-12 23:05:22 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,8 +5,7 @@ class Application
 
   # first,
   def call(env)
-    # create a rack response
-    # and a rack request
+
     binding.pry
 
     req = Rack::Request.new(env)
@@ -17,9 +16,6 @@ class Application
     else
       resp.write "Route not found"
       resp.status = 404
-
   end
-
-# app should accept /items/<item NAME> route
 
 end
